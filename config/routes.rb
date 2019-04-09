864da4d034ad4bd664bc125e1201fcbf50e248d0
Martin <martin.st8n@gmail.com>
2019-04-09 14:18:45 -0400

    create user model, added bcrypt and login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "users#index"
-  resources :users
+  # resources :users
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 end
