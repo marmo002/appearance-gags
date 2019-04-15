4f7edd08ebb25eb9518533c77f89116975af391f
Martin <martin.st8n@gmail.com>
2019-04-15 10:43:07 -0400

    Image uploading validation: Added minimagic gem, used ActiveStorage::Analyzer::ImageAnalyzer.new(blob) to get metadata before file is save, and verify width is at least 700px and up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  User.create(
+    first_name: 'Martin',
+    last_name: "Martin",
+    email: "martin@mail.com",
+    phone: "(416) 939 6469",
+    password: "password",
+    password_confirmation: "password"
+  )
