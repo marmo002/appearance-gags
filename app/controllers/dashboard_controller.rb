a2753b26f42265c3ca92fc5a0e5b2a2b24a3a184
Martin <martin.st8n@gmail.com>
2019-04-16 10:46:04 -0400

    Fix avatar and companylogo validation where it'll require an image on every update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_action :require_login
-  before_action :authorized?, only: [ :admin_dashboard ]
+  before_action :authorized?, only: [ :admin_dashboard, :company_update ]
 
   def index
     if is_admin?
@@ -9,7 +9,7 @@ class DashboardController < ApplicationController
   end
 
   def admin_dashboard
-
+    @users = User.where.not(id: current_user.id).take(20)
   end
 
   def user_update
@@ -19,6 +19,17 @@ class DashboardController < ApplicationController
     else
       flash[:danger] = "Please review form"
       session[:user_errors] = current_user.errors.as_json(full_messages: true)
+      redirect_to dashboard_url
+    end
+  end
+
+  def company_update
+    if company.update(company_params)
+      flash[:primary] = "New info was saved"
+      redirect_to dashboard_url
+    else
+      flash[:danger] = "Please review form"
+      session[:user_errors] = company.errors.as_json(full_messages: true)
       redirect_to dashboard_url
     end
   end
@@ -42,6 +53,21 @@ private
       :companylogo,
       social_media: [ :profile, :facebook, :linkedin, :twitter, :instagram, :other ],
       company_social_media: [ :website, :facebook, :linkedin, :twitter, :instagram, :other ]
+    )
+  end
+
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :address1,
+      :address2,
+      :city,
+      :province,
+      :postal_code,
+      :email,
+      :phone,
+      :fax,
+      :release
     )
   end
 
