ad5e9176d02c7c10c96f33da0a1c57ccac707103
Martin <martin.st8n@gmail.com>
2019-04-23 13:30:28 -0400

    Got google calendar api working with google-api-client CalendarV3

--- app/controllers/calendarapi_controller.rb
+++ app/controllers/calendarapi_controller.rb
@@ -1,18 +1,37 @@
 class CalendarapiController < ApplicationController
   KEY_FILE_PATH = Rails.application.credentials[:calendar_api][:key_file]
+  GOOGLE_CREDENTIALS = ENV.fetch('GOOGLE_APPLICATION_CREDENTIALS')
+
+  # require 'googleauth'
+  # require 'google/apis/calendar_v3'
 
   def index
-    @text = "Calendar page"
-# @key_file = ENV['GOOGLE_APPLICATION_CREDENTIALS']
-@key_file = KEY_FILE_PATH
-    # calendar = Google::Apis::CalendarV3::CalendarService.new
-    # calendar.authorization = credentials_for
-    # calendar_id = 'primary'
-    # @result = calendar.list_events(calendar_id,
-    #                                max_results: 10,
-    #                                single_events: true,
-    #                                order_by: 'startTime',
-    #                                time_min: Time.now.iso8601)
+    calendar = Google::Apis::CalendarV3::CalendarService.new
+    scopes =  ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/calendar']
+    calendar.authorization = Google::Auth.get_application_default(scopes)
+    # token = calendar.authorization.fetch_access_token!
+
+    emails = ["me@example.com","myboss@example.com"]
+    # Create an event, adding any emails listed in the command line as attendees
+    # event = Calendar::Event.new(
+    #   summary: 'A sample event',
+    #   location: '1600 Amphitheatre Parkway, Mountain View, CA 94045',
+    #   attendees:  emails.each { |email| Calendar::EventAttendee.new(email: email) },
+    #   start: Calendar::EventDateTime.new(date_time: DateTime.parse('2015-12-31T20:00:00')),
+    #   end: Calendar::EventDateTime.new(date_time: DateTime.parse('2016-01-01T02:00:00'))
+    # )
+    # event = calendar.insert_event('primary', event, send_notifications: true)
+    # puts "Created event '#{event.summary}' (#{event.id})"
+
+    calendar_id = 'getagripmatt@gmail.com'
+    @response = calendar.list_events(
+        calendar_id,
+        max_results: 10,
+        single_events: true,
+        order_by: 'startTime',
+        time_min: Time.now.iso8601
+      )
+
   end
 
   def credentials_for
@@ -23,6 +42,11 @@ class CalendarapiController < ApplicationController
     # credentials = authorizer.fetch_access_token!
     #
     # credentials
+  end
+
+  private
+
+  def calendar_init
   end
 
 end #controlled end
