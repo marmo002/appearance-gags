c568af96b3c700b0f81310c1434c6881f5d00980
Martin <martin.st8n@gmail.com>
2019-04-26 09:32:13 -0400

    removed phone validation on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, :phone, presence: true
   validates :email, uniqueness: true
-  validates :phone, format:{
-    with: /\A\(\d{3}\).\d{3}.\d{4}\z/,
-    message: 'must be in format (123) 456 7878'
-  }
+  # validates :phone, format:{
+  #   with: /\A\(\d{3}\).\d{3}.\d{4}\z/,
+  #   message: 'must be in format (123) 456 7878'
+  # }
   validates :password, length: { minimum: 6 }, on: :create
 
-  validate :image_validation, on: :update
+  # validate :image_validation, on: :update
 
   default_scope { order(id: :desc) }
 
