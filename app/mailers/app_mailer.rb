e1a8e1b26239e6a853d8c0a2ae83080a2af90148
Martin <martin.st8n@gmail.com>
2019-05-03 12:24:52 -0400

    Added email template to mailer for better email display

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,7 +2,7 @@ class AppMailer < ApplicationMailer
 
   def send_release_copy(user, company_release)
     @user = user
-    @release = company_release
+    @company = Company.first
     mail(to: @user.email, subject: 'Your copy of our release')
   end
 end
