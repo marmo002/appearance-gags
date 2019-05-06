601cdc402e570e88100efa96c640f6339617d197
Martin <martin.st8n@gmail.com>
2019-05-06 07:13:04 -0400

    added send_release_copy on welcome form controller to send email after user has finished profile

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,7 @@
 class AppMailer < ApplicationMailer
 
-  def send_release_copy(user, company_release)
-    @user = user
+  def send_release_copy(user_id)
+    @user = User.find(user_id)
     @company = Company.first
     mail(to: @user.email, subject: 'Your copy of our release')
   end
